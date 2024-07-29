b261d66c62ebb45a50b3cbdda139d6f3ef76e24d
wavymejti <mateusztheszymkowiak@gmail.com>
2024-07-29 20:52:58 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,21 @@
-a = 5
-b = 7
-c = a + b
-print(c)
+#definiowanie liczb
+a = float(input("podaj liczbe a\n")) #definiowanie liczby a
+b = float(input("podaj liczbe b\n")) #definiowanie liczby b
+
+#dodawanie liczb całkowitych
+dodawaniecal = int(a + b) #operacja arytmetyczna na liczbach całkowitych
+print(dodawaniecal)
+
+#dodawanie liczb rzeczywistych
+dodawaniezmien = float(a+b)
+print(dodawaniezmien)
+
+#odejmowanie liczb całkowitch
+odejmowaniecal = int(a - b)
+print(odejmowaniecal)
+
+#odejmowanie liczb rzeczywistch 
+odejmowanierzeczywistych = float(a - b)
+print(odejmowanierzeczywistych)
+
+
