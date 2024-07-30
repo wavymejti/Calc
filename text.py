1d683a729ea578af29f588c27bc98520a65279f6
wavymejti <mateusztheszymkowiak@gmail.com>
2024-07-30 21:34:09 +0200

    Update text.py

--- text.py
+++ text.py
@@ -7,3 +7,21 @@ spliter = user.split()
 print(spliter)
 
 #getting variables
+x = int(spliter[0])
+y = int(spliter[2])
+operator = spliter[1]
+
+def dodawanie(x, y):
+    wynikdodawania = x + y
+    return wynikdodawania
+
+def odejmowanie(x, y):
+    wynikodejmowania = x - y
+    return wynikodejmowania
+
+if operator == "+":
+    print(dodawanie(x, y))
+elif operator == "-":
+    print(odejmowanie(x, y))
+else:
+    print("nieznany operator")
